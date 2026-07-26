038c5b453d4b1c4667868a8a8aac43853e9b13fa
vinayintipalli <vinayintipalli@users.noreply.github.com>
2026-07-27 00:34:44 +0530

    Removed Pdfs and Simple examples

--- simple_examples/Compilation_Model/math_utils.cpp
+++
@@ -1,5 +0,0 @@
-#include "math_utils.hpp"
-
-int square(int x) {   // DEFINITION lives here
-    return x * x;
-}
--- simple_examples/Compilation_Model/math_utils.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef MATH_UTILS_H
-#define MATH_UTILS_H
-
-int square(int x);   // DECLARATION only
-
-#endif
